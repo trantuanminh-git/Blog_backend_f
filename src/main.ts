6d90a40fd34e8640f86af9cd628828a2776780da
trantuanminh-git <trantuanminh9802@gmail.com>
2023-03-10 16:16:28 +0700

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://54.179.238.121:3000',
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: '*',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
