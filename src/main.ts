078932a218f9de7165bd0d278f66da1f43c90cc7
trantuanminh-git <trantuanminh9802@gmail.com>
2023-03-11 15:47:19 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://54.179.238.121:3000',
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+    // origin: 'http://54.179.238.121:3000',
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: '*',
     credentials: true,
   });
