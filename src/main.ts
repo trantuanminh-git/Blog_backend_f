5e66cea8703cc212ac08c104bb823bbf90fa7845
NguyenMinhThang <thang1982001@gmail.com>
2023-03-12 13:48:32 +0700

    add Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
   app.enableCors({
     // origin: 'http://54.179.238.121:3000',
     origin: '*',
