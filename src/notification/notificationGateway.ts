3e390eaf250b41c14516f9a986356a80bf56d91a
TranManhSon <83875162+TranManhSon@users.noreply.github.com>
2023-03-25 14:14:27 +0700

    check socket

--- src/notification/notificationGateway.ts
+++ src/notification/notificationGateway.ts
@@ -1,36 +1,36 @@
-import { WsGuard } from './../common/guards/ws.guard';
-import { UseGuards, HttpException, HttpStatus } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { ConnectedSocket, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-import { BlogService } from 'src/blog/blog.service';
-import { User } from 'src/user/entities/user.entity';
-@WebSocketGateway({ cors : true})
-export class NotificationGateway {
-  @WebSocketServer()
-  server: Server;
+// import { WsGuard } from './../common/guards/ws.guard';
+// import { UseGuards, HttpException, HttpStatus } from '@nestjs/common';
+// import { JwtService } from '@nestjs/jwt';
+// import { ConnectedSocket, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+// import { Server, Socket } from 'socket.io';
+// import { BlogService } from 'src/blog/blog.service';
+// import { User } from 'src/user/entities/user.entity';
+// @WebSocketGateway({ cors : true})
+// export class NotificationGateway {
+//   @WebSocketServer()
+//   server: Server;
 
-  constructor(
-    private blogService: BlogService,
-  ) {}
+//   constructor(
+//     private blogService: BlogService,
+//   ) {}
 
-  handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
-  }
+//   handleConnection(client: Socket) {
+//     console.log(`Client connected: ${client.id}`);
+//   }
 
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
+//   handleDisconnect(client: Socket) {
+//     console.log(`Client disconnected: ${client.id}`);
+//   }
 
-  @UseGuards(WsGuard)
-  @SubscribeMessage('notification')
-  receivedAndSendNotificationToUser(client: Socket, message: string): void {
-    console.log("server received notification")
-    const data = message.split(' ', 2);
+//   @UseGuards(WsGuard)
+//   @SubscribeMessage('notification')
+//   receivedAndSendNotificationToUser(client: Socket, message: string): void {
+//     console.log("server received notification")
+//     const data = message.split(' ', 2);
 
-    const userId = this.blogService.findUserIdByBlogId(parseInt(data[1]))
-    // this.server.on(`client_${client.id}`).emit('notification', message);
-    const noti = `You have one ${data[1]}`;
-    this.server.to(`client_${userId}`).emit('notification', noti);
-  }
-}
+//     const userId = this.blogService.findUserIdByBlogId(parseInt(data[1]))
+//     // this.server.on(`client_${client.id}`).emit('notification', message);
+//     const noti = `You have one ${data[1]}`;
+//     this.server.to(`client_${userId}`).emit('notification', noti);
+//   }
+// }
--- src/rating/rating.controller.ts
+++ src/rating/rating.controller.ts
@@ -18,7 +18,7 @@ import { AtGuard } from 'src/common/guards/at.guard';
 import { NotificationService } from 'src/notification/notification.service';
 import { NotificationType } from 'src/notification/entities/notification.entity';
 import { Notification } from 'src/notification/entities/notification.entity';
-import { NotificationGateway } from 'src/notification/notificationGateway';
+// import { NotificationGateway } from 'src/notification/notificationGateway';
 @Controller()
 export class RatingController {
   constructor(private readonly ratingService: RatingService){}
