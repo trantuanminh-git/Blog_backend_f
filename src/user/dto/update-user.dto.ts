5c33b6d0824f92651537c443d979b7de1a0aa401
Thang Nguyen Minh <thang@thangnguyen.com>
2023-05-05 14:34:01 +0700

    add change role of user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Role } from 'src/role/entities/role.entity';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   email: string;
   username: string;
   password: string;
   biography: string;
+  role: Role;
 }
